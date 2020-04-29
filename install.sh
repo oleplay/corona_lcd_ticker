781cd058a33567578d63d339acb11f28a28c1a6a
oleplay <ole.schicketanz@gmx.de>
2020-04-29 18:15:14 +0200

    Complete Install Script

--- install.sh
+++ install.sh
@@ -1,6 +1,16 @@
-sudo apt install libxslt-dev python3 python3-venv python3-pip
+#/bin/bash
+
+sudo apt install libxslt-dev python3 python3-venv python3-pip -y
 
 python3 -m venv Scraper/python/
 
-pip install pySerial Scrapy
+source Scraper/python/bin/activate
 
+pip install pySerial Scrapy RPi.GPIO
+
+sudo tee /etc/cron.d/corona <<-EOF
+@reboot /bin/bash "/home/pi/corona/output_button_loop.sh" > /home/pi/corona_loop.log
+*/5 * * * * /bin/bash "/home/pi/corona/output.sh" > /home/pi/corona.log
+EOF
+
+sudo reboot
